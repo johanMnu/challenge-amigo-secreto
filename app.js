c46c58f4862bab517f9a5d1a8ca3504dd5f4a0a6
johanMnu <johanboca@hotmail.com>
2025-01-21 21:20:32 -0300

    primer paso

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-function cambiarValor(){
+let listaAmigos = [];
+function Amigos (){
+    let nombreAmigo = document.getElementById("amigo").value;
+    if(nombreAmigo == ""){
+        alert("por favor inserte un nombre");
 
-};
+    } else{
+        listaAmigos.push(nombreAmigo);
+        nombreAmigo = document.getElementById("amigo").value="";
+    }
+}
